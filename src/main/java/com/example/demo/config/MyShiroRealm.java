6420d6bb82e6425199a80b0da8361da840e457c8
屈雄伟 <qu.xiongwei@cesgroup.com.cn>
2017-08-10 15:51:52 +0800

    提交 了多个方法，包含表单校验，异常处理，公共类的管理等等

--- src/main/java/com/example/demo/config/MyShiroRealm.java
+++ src/main/java/com/example/demo/config/MyShiroRealm.java
@@ -33,10 +33,22 @@ private UserService userService;
         //通过username从数据库中查找 User对象，如果找到，没找到.
         //实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
         User user = userService.findByUsername(username);
+        /*
+       *//*
+        * 获取权限信息:这里没有进行实现，
+        * 请自行根据UserInfo,Role,Permission进行实现；
+        * 获取之后可以在前端for循环显示所有链接;
+        *//*
+        //userInfo.setPermissions(userService.findPermissions(user));
+        */
         System.out.println("----->>user="+user);
         if(user == null){
             return null;
         }
+        //账号判断;
+
+        //加密方式;
+        //交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配，如果觉得人家的不好可以自定义实现
         SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
                         user, //用户名
                         user.getPassword(), //密码
@@ -44,46 +56,7 @@ private UserService userService;
                         getName()  //realm name
                 );
     return authenticationInfo ;
-/*
-        //获取用户的输入的账号.
-        String username = (String)token.getPrincipal();
-        System.out.println(token.getCredentials());
 
-        //通过username从数据库中查找 User对象，如果找到，没找到.
-        //实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
-        User userInfo = userService.findByUsername(username);
-        System.out.println("----->>userInfo="+userInfo);
-        if(userInfo == null){
-            return null;
-        }
-
-       *//*
-        * 获取权限信息:这里没有进行实现，
-        * 请自行根据UserInfo,Role,Permission进行实现；
-        * 获取之后可以在前端for循环显示所有链接;
-        *//*
-        //userInfo.setPermissions(userService.findPermissions(user));
-
-
-        //账号判断;
-
-        //加密方式;
-        //交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配，如果觉得人家的不好可以自定义实现
-        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
-                userInfo, //用户名
-                userInfo.getPassword(), //密码
-                ByteSource.Util.bytes(userInfo.getCredentialsSalt()),//salt=username+salt
-                getName()  //realm name
-        );
-
-        //明文: 若存在，将此用户存放到登录认证info中，无需自己做密码对比，Shiro会为我们进行密码对比校验
-//      SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
-//           userInfo, //用户名
-//           userInfo.getPassword(), //密码
-//             getName()  //realm name
-//      );
-
-        return authenticationInfo;*/
     }
 
 
